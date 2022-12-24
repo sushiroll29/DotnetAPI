9e7d02fbe86063f83b5854a5446a672fcebe640f
sushiroll29 <ale.surdu29@gmail.com>
2022-12-24 16:35:28 +0100

    added dto for user

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -74,7 +74,7 @@ public class UserController : ControllerBase
     }
 
     [HttpPost("AddUser")]
-    public IActionResult AddUser(User user)
+    public IActionResult AddUser(UserToAddDTO user)
     {
         string sql = @"
         INSERT INTO TutorialAppSchema.Users(
